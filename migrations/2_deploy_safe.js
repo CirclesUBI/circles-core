948138a1a4280a4080b3380b91290169ee433b34
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-09-02 11:30:31 +0200

    Use zero address for setup method

--- migrations/2_deploy_safe.js
+++ migrations/2_deploy_safe.js
@@ -8,11 +8,13 @@ const ProxyFactory = artifacts.require(
   '@gnosis.pm/safe-contracts/ProxyFactory.sol',
 );
 
+const ZERO_ADDRESS = '0x00000000000000000000000000000000';
+
 module.exports = async (deployer, network, accounts) => {
   await deployer.deploy(ProxyFactory);
 
   await deployer.deploy(GnosisSafe).then(safe => {
-    safe.setup([accounts[0]], 1, '0x', 0, 0, 0, 0);
+    safe.setup([accounts[0]], 1, ZERO_ADDRESS, 0, 0, 0, 0);
     return safe;
   });
 };
