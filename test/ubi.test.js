767664918c3ef045e37ead1b47d647110d5abb5d
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-09-30 14:22:19 +0200

    Implement getBalance and getTokenAddress methods

--- test/ubi.test.js
+++ test/ubi.test.js
@@ -2,6 +2,9 @@ import createCore from './helpers/createCore';
 import getAccount from './helpers/getAccount';
 import web3 from './helpers/web3';
 
+// This was set during Hub deployment:
+const INITIAL_PAYOUT = 100;
+
 let account;
 let core;
 let safeAddress;
@@ -39,6 +42,29 @@ describe('UBI', () => {
       await core.ubi.signup(account, {
         safeAddress,
       });
+    });
+  });
+
+  describe('when a user wants to interact with its Token', () => {
+    let tokenAddress;
+
+    beforeAll(async () => {
+      tokenAddress = await core.ubi.getTokenAddress(account, {
+        safeAddress,
+      });
+    });
+
+    it('should be a valid contract address', async () => {
+      expect(web3.eth.getCode(tokenAddress)).not.toBe('0x');
+    });
+
+    it('should get the initial payout balance', async () => {
+      const balance = await core.ubi.getBalance(account, {
+        safeAddress,
+        tokenAddress,
+      });
+
+      expect(parseInt(balance, 10)).toBe(INITIAL_PAYOUT);
     });
   });
 });
