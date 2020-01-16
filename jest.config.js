ab78b79acfbafa5be7d94a1b4b4be5dc5184814f
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-01-16 18:50:18 +0100

    Change back to higher test timeout

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   testEnvironment: 'node',
-  testTimeout: 120 * 1000,
+  testTimeout: 360 * 1000,
 
   // Resolve modules with alias
   moduleNameMapper: {
