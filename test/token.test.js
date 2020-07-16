a895bf25630a383a1ed57360f569ad55c04fc718
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-07-16 13:46:06 +0200

    Fix token test by redeploying trust network

--- test/token.test.js
+++ test/token.test.js
@@ -12,6 +12,58 @@ import {
   addTrustConnection,
 } from './helpers/transactions';
 
+const TEST_TRUST_NETWORK = [
+  [0, 1, 25],
+  [1, 0, 50],
+  [1, 2, 10],
+  [2, 1, 20],
+  [2, 3, 5],
+  [3, 2, 15],
+  [3, 0, 25],
+  [3, 4, 25],
+  [4, 3, 15],
+  [4, 1, 10],
+  [2, 5, 50], // Unidirectional
+];
+
+async function deployTestNetwork(
+  core,
+  accounts,
+  connections = TEST_TRUST_NETWORK,
+) {
+  // Deploy Safe and Token for each test account
+  const tasks = accounts.map((account) => {
+    return deploySafeAndToken(core, account);
+  });
+
+  const results = await Promise.all(tasks);
+
+  const safeAddresses = [];
+  const tokenAddresses = [];
+
+  results.forEach((result) => {
+    const { safeAddress, tokenAddress } = result;
+
+    safeAddresses.push(safeAddress);
+    tokenAddresses.push(tokenAddress);
+  });
+
+  const connectionTasks = connections.map((connection) => {
+    return addTrustConnection(core, accounts[connection[0]], {
+      user: safeAddresses[connection[1]],
+      canSendTo: safeAddresses[connection[0]],
+      limitPercentage: connection[2],
+    });
+  });
+
+  await Promise.all(connectionTasks);
+
+  return {
+    safeAddresses,
+    tokenAddresses,
+  };
+}
+
 describe('Token', () => {
   let core;
   let utils;
@@ -38,153 +90,12 @@ describe('Token', () => {
     });
   });
 
-  describe('Transitive Transactions', () => {
+  describe('Calculated transitive transactions', () => {
     let safeAddresses;
-    let tokenAddresses;
 
     beforeAll(async () => {
-      // Deploy Safe and Token for each test account
-      const tasks = accounts.map((account) => {
-        return deploySafeAndToken(core, account);
-      });
-
-      const results = await Promise.all(tasks);
-
-      safeAddresses = [];
-      tokenAddresses = [];
-
-      results.forEach((result) => {
-        const { safeAddress, tokenAddress } = result;
-
-        safeAddresses.push(safeAddress);
-        tokenAddresses.push(tokenAddress);
-      });
-
-      // Create trust connections
-      const connections = [
-        [0, 1, 25],
-        [1, 0, 50],
-        [1, 2, 10],
-        [2, 1, 20],
-        [2, 3, 5],
-        [3, 2, 15],
-        [3, 0, 25],
-        [3, 4, 25],
-        [4, 3, 15],
-        [4, 1, 10],
-        [2, 5, 50], // Unidirectional
-      ];
-
-      const connectionTasks = connections.map((connection) => {
-        return addTrustConnection(core, accounts[connection[0]], {
-          user: safeAddresses[connection[1]],
-          canSendTo: safeAddresses[connection[0]],
-          limitPercentage: connection[2],
-        });
-      });
-
-      await Promise.all(connectionTasks);
-    });
-
-    it('should get the current balance', async () => {
-      const balance = await core.token.getBalance(accounts[5], {
-        safeAddress: safeAddresses[5],
-      });
-
-      // It should be equals the initial UBI payout
-      // which was set during Hub contract deployment:
-      expect(balance).toMatchObject(
-        new web3.utils.BN(core.utils.toFreckles(100)),
-      );
-    });
-
-    it('should send Circles to someone directly', async () => {
-      const value = web3.utils.toBN(core.utils.toFreckles(5));
-
-      // Unidirectional trust relationship from 2 to 5
-      const indexFrom = 5;
-      const indexTo = 2;
-
-      // Transfer from 5 to 2
-      const response = await core.token.transfer(accounts[indexFrom], {
-        from: safeAddresses[indexFrom],
-        to: safeAddresses[indexTo],
-        value,
-      });
-
-      expect(web3.utils.isHexStrict(response)).toBe(true);
-    });
-
-    it('should send Circles to someone transitively', async () => {
-      const value = web3.utils.toBN(core.utils.toFreckles(5));
-      const indexFrom = 0;
-      const indexTo = 4;
-
-      const response = await core.token.transfer(accounts[indexFrom], {
-        from: safeAddresses[indexFrom],
-        to: safeAddresses[indexTo],
-        value,
-      });
-
-      expect(web3.utils.isHexStrict(response)).toBe(true);
-
-      const accountBalance = await loop(
-        () => {
-          return core.token.getBalance(accounts[indexFrom], {
-            safeAddress: safeAddresses[indexFrom],
-          });
-        },
-        (balance) => balance.toString().slice(0, 2) === '94',
-      );
-
-      const otherAccountBalance = await core.token.getBalance(
-        accounts[indexTo],
-        {
-          safeAddress: safeAddresses[indexTo],
-        },
-      );
-
-      expect(otherAccountBalance.toString().slice(0, 3)).toBe('104');
-      expect(accountBalance.toString().slice(0, 2)).toBe('94');
-    });
-
-    it('should fail sending Circles when there is no path', async () => {
-      // Max flow is smaller than the given transfer value.
-      await expect(
-        core.token.transfer(accounts[0], {
-          from: safeAddresses[0],
-          to: safeAddresses[4],
-          value: web3.utils.toBN(core.utils.toFreckles('100')),
-        }),
-      ).rejects.toThrow();
-
-      // Trust connection does not exist between
-      // node 0 and 5
-      await expect(
-        core.token.transfer(accounts[0], {
-          from: safeAddresses[0],
-          to: safeAddresses[5],
-          value: web3.utils.toBN('1'),
-        }),
-      ).rejects.toThrow();
-    });
-
-    describe('getNetwork', () => {
-      it('should return the correct trust network', async () => {
-        const connection = await loop(async () => {
-          const network = await getNetwork(web3, utils, {
-            from: safeAddresses[0],
-            to: safeAddresses[4],
-            networkHops: 3,
-          });
-
-          return network.find(({ from, to }) => {
-            return from === safeAddresses[2] && to === safeAddresses[1];
-          });
-        }, isReady);
-
-        expect(connection.tokenOwnerAddress).toBe(safeAddresses[2]);
-      });
+      const result = await deployTestNetwork(core, accounts);
+      safeAddresses = result.safeAddresses;
     });
 
     describe('calculateTransfer', () => {
@@ -200,7 +111,7 @@ describe('Token', () => {
         expect(result.transactionsPath.length).toBe(0);
 
         expect(result.maximumFlow.toString()).toBe(
-          web3.utils.toWei('20', 'ether'),
+          web3.utils.toWei('25', 'ether'),
         );
       });
 
@@ -216,141 +127,255 @@ describe('Token', () => {
         expect(result.transactionsPath.length).toBe(2);
 
         expect(result.transactionsPath[0].from).toBe(safeAddresses[0]);
-        expect(result.transactionsPath[0].to).toBe(safeAddresses[1]);
+        expect(result.transactionsPath[0].to).toBe(safeAddresses[3]);
         expect(result.transactionsPath[0].value).toBe(value.toString());
         expect(result.transactionsPath[0].tokenOwnerAddress).toBe(
           safeAddresses[0],
         );
-        expect(result.transactionsPath[1].from).toBe(safeAddresses[1]);
+        expect(result.transactionsPath[1].from).toBe(safeAddresses[3]);
         expect(result.transactionsPath[1].to).toBe(safeAddresses[4]);
         expect(result.transactionsPath[1].value).toBe(value.toString());
         expect(result.transactionsPath[1].tokenOwnerAddress).toBe(
-          safeAddresses[1],
+          safeAddresses[3],
         );
 
         expect(result.maximumFlow.toString()).toBe(
-          web3.utils.toWei('20', 'ether'),
+          web3.utils.toWei('25', 'ether'),
         );
       });
     });
+  });
 
-    describe('findTransitiveTransactions', () => {
-      const NUM_NODES = 8;
-      const INDEX_SENDER = 0;
-      const INDEX_RECEIVER = 7;
+  describe('Transitive Transactions', () => {
+    let safeAddresses;
+    let tokenAddresses;
 
-      let nodes;
-      let network;
-
-      beforeEach(() => {
-        nodes = new Array(NUM_NODES).fill('').map(() => {
-          return web3.utils.toChecksumAddress(web3.utils.randomHex(20));
-        });
-
-        network = [
-          { from: nodes[0], to: nodes[1], capacity: 10 },
-          { from: nodes[0], to: nodes[2], capacity: 5 },
-          { from: nodes[0], to: nodes[3], capacity: 15 },
-          { from: nodes[1], to: nodes[4], capacity: 9 },
-          { from: nodes[1], to: nodes[5], capacity: 15 },
-          { from: nodes[1], to: nodes[2], capacity: 4 },
-          { from: nodes[2], to: nodes[5], capacity: 8 },
-          { from: nodes[2], to: nodes[3], capacity: 4 },
-          { from: nodes[3], to: nodes[6], capacity: 16 },
-          { from: nodes[4], to: nodes[5], capacity: 15 },
-          { from: nodes[4], to: nodes[7], capacity: 10 },
-          { from: nodes[5], to: nodes[7], capacity: 10 },
-          { from: nodes[5], to: nodes[6], capacity: 15 },
-          { from: nodes[6], to: nodes[2], capacity: 6 },
-          { from: nodes[6], to: nodes[7], capacity: 10 },
-        ];
-
-        network.map((connection) => {
-          connection.capacity = web3.utils.toBN(
-            core.utils.toFreckles(connection.capacity),
-          );
-
-          connection.tokenOwnerAddress = web3.utils.toChecksumAddress(
-            web3.utils.randomHex(20),
-          );
-
-          return connection;
-        });
-      });
-
-      it('should fail when max-flow is too small', () => {
-        const value = new web3.utils.BN(core.utils.toFreckles(100));
-
-        expect(() => {
-          findTransitiveTransactions(web3, utils, {
-            from: nodes[INDEX_SENDER],
-            to: nodes[INDEX_RECEIVER],
-            value,
-            network,
-          });
-        }).toThrow();
-      });
-
-      it('should successfully find a path', () => {
-        for (let i = 0; i < 10; i += 1) {
-          const value = 1 + Math.round(Math.random() * 27);
-
-          const path = findTransitiveTransactions(web3, utils, {
-            from: nodes[INDEX_SENDER],
-            to: nodes[INDEX_RECEIVER],
-            value: new web3.utils.BN(core.utils.toFreckles(value)),
-            network,
-          });
-
-          // Simulate transaction
-          const balances = new Array(NUM_NODES).fill(0);
-          balances[INDEX_SENDER] = value;
-
-          path.forEach((transaction) => {
-            const indexFrom = nodes.indexOf(transaction.from);
-            const indexTo = nodes.indexOf(transaction.to);
-
-            balances[indexFrom] -= core.utils.fromFreckles(transaction.value);
-            balances[indexTo] += core.utils.fromFreckles(transaction.value);
-          });
-
-          expect(balances[INDEX_RECEIVER]).toBe(value);
-        }
-      });
+    beforeAll(async () => {
+      const result = await deployTestNetwork(core, accounts);
+      safeAddresses = result.safeAddresses;
+      tokenAddresses = result.tokenAddresses;
     });
 
-    describe('requestUBIPayout', () => {
-      let token;
-      let payout;
-
-      beforeAll(async () => {
-        token = await getTokenContract(web3, tokenAddresses[5]);
-
-        payout = await core.token.checkUBIPayout(accounts[5], {
+    describe('Transitive transactions', () => {
+      it('should get the current balance', async () => {
+        const balance = await core.token.getBalance(accounts[5], {
           safeAddress: safeAddresses[5],
+        });
+
+        // It should be equals the initial UBI payout
+        // which was set during Hub contract deployment:
+        expect(balance).toMatchObject(
+          new web3.utils.BN(core.utils.toFreckles(100)),
+        );
+      });
+
+      it('should send Circles to someone directly', async () => {
+        const value = web3.utils.toBN(core.utils.toFreckles(5));
+
+        // Unidirectional trust relationship from 2 to 5
+        const indexFrom = 5;
+        const indexTo = 2;
+
+        // Transfer from 5 to 2
+        const response = await core.token.transfer(accounts[indexFrom], {
+          from: safeAddresses[indexFrom],
+          to: safeAddresses[indexTo],
+          value,
+        });
+
+        expect(web3.utils.isHexStrict(response)).toBe(true);
+      });
+
+      it('should send Circles to someone transitively', async () => {
+        const value = web3.utils.toBN(core.utils.toFreckles(5));
+        const indexFrom = 0;
+        const indexTo = 4;
+
+        const response = await core.token.transfer(accounts[indexFrom], {
+          from: safeAddresses[indexFrom],
+          to: safeAddresses[indexTo],
+          value,
+        });
+
+        expect(web3.utils.isHexStrict(response)).toBe(true);
+
+        const accountBalance = await loop(
+          () => {
+            return core.token.getBalance(accounts[indexFrom], {
+              safeAddress: safeAddresses[indexFrom],
+            });
+          },
+          (balance) => balance.toString().slice(0, 2) === '94',
+        );
+
+        const otherAccountBalance = await core.token.getBalance(
+          accounts[indexTo],
+          {
+            safeAddress: safeAddresses[indexTo],
+          },
+        );
+
+        expect(otherAccountBalance.toString().slice(0, 3)).toBe('104');
+        expect(accountBalance.toString().slice(0, 2)).toBe('94');
+      });
+
+      it('should fail sending Circles when there is no path', async () => {
+        // Max flow is smaller than the given transfer value.
+        await expect(
+          core.token.transfer(accounts[0], {
+            from: safeAddresses[0],
+            to: safeAddresses[4],
+            value: web3.utils.toBN(core.utils.toFreckles('100')),
+          }),
+        ).rejects.toThrow();
+
+        // Trust connection does not exist between
+        // node 0 and 5
+        await expect(
+          core.token.transfer(accounts[0], {
+            from: safeAddresses[0],
+            to: safeAddresses[5],
+            value: web3.utils.toBN('1'),
+          }),
+        ).rejects.toThrow();
+      });
+
+      describe('getNetwork', () => {
+        it('should return the correct trust network', async () => {
+          const connection = await loop(async () => {
+            const network = await getNetwork(web3, utils, {
+              from: safeAddresses[0],
+              to: safeAddresses[4],
+              networkHops: 3,
+            });
+
+            return network.find(({ from, to }) => {
+              return from === safeAddresses[2] && to === safeAddresses[1];
+            });
+          }, isReady);
+
+          expect(connection.tokenOwnerAddress).toBe(safeAddresses[2]);
         });
       });
 
-      it('should add the next payout to our balance', async () => {
-        const balanceBefore = await token.methods
-          .balanceOf(safeAddresses[5])
-          .call();
+      describe('findTransitiveTransactions', () => {
+        const NUM_NODES = 8;
+        const INDEX_SENDER = 0;
+        const INDEX_RECEIVER = 7;
 
-        await core.token.requestUBIPayout(accounts[5], {
-          safeAddress: safeAddresses[5],
+        let nodes;
+        let network;
+
+        beforeEach(() => {
+          nodes = new Array(NUM_NODES).fill('').map(() => {
+            return web3.utils.toChecksumAddress(web3.utils.randomHex(20));
+          });
+
+          network = [
+            { from: nodes[0], to: nodes[1], capacity: 10 },
+            { from: nodes[0], to: nodes[2], capacity: 5 },
+            { from: nodes[0], to: nodes[3], capacity: 15 },
+            { from: nodes[1], to: nodes[4], capacity: 9 },
+            { from: nodes[1], to: nodes[5], capacity: 15 },
+            { from: nodes[1], to: nodes[2], capacity: 4 },
+            { from: nodes[2], to: nodes[5], capacity: 8 },
+            { from: nodes[2], to: nodes[3], capacity: 4 },
+            { from: nodes[3], to: nodes[6], capacity: 16 },
+            { from: nodes[4], to: nodes[5], capacity: 15 },
+            { from: nodes[4], to: nodes[7], capacity: 10 },
+            { from: nodes[5], to: nodes[7], capacity: 10 },
+            { from: nodes[5], to: nodes[6], capacity: 15 },
+            { from: nodes[6], to: nodes[2], capacity: 6 },
+            { from: nodes[6], to: nodes[7], capacity: 10 },
+          ];
+
+          network.map((connection) => {
+            connection.capacity = web3.utils.toBN(
+              core.utils.toFreckles(connection.capacity),
+            );
+
+            connection.tokenOwnerAddress = web3.utils.toChecksumAddress(
+              web3.utils.randomHex(20),
+            );
+
+            return connection;
+          });
         });
 
-        const balanceAfter = await token.methods
-          .balanceOf(safeAddresses[5])
-          .call();
+        it('should fail when max-flow is too small', () => {
+          const value = new web3.utils.BN(core.utils.toFreckles(100));
 
-        const expectedBalance = web3.utils
-          .toBN(balanceBefore)
-          .add(payout)
-          .toString();
+          expect(() => {
+            findTransitiveTransactions(web3, utils, {
+              from: nodes[INDEX_SENDER],
+              to: nodes[INDEX_RECEIVER],
+              value,
+              network,
+            });
+          }).toThrow();
+        });
 
-        // Do not check for the exact amount as payout is changing every second
-        expect(web3.utils.toBN(balanceAfter).gt(expectedBalance)).toBe(true);
+        it('should successfully find a path', () => {
+          for (let i = 0; i < 10; i += 1) {
+            const value = 1 + Math.round(Math.random() * 27);
+
+            const path = findTransitiveTransactions(web3, utils, {
+              from: nodes[INDEX_SENDER],
+              to: nodes[INDEX_RECEIVER],
+              value: new web3.utils.BN(core.utils.toFreckles(value)),
+              network,
+            });
+
+            // Simulate transaction
+            const balances = new Array(NUM_NODES).fill(0);
+            balances[INDEX_SENDER] = value;
+
+            path.forEach((transaction) => {
+              const indexFrom = nodes.indexOf(transaction.from);
+              const indexTo = nodes.indexOf(transaction.to);
+
+              balances[indexFrom] -= core.utils.fromFreckles(transaction.value);
+              balances[indexTo] += core.utils.fromFreckles(transaction.value);
+            });
+
+            expect(balances[INDEX_RECEIVER]).toBe(value);
+          }
+        });
+      });
+
+      describe('requestUBIPayout', () => {
+        let token;
+        let payout;
+
+        beforeAll(async () => {
+          token = await getTokenContract(web3, tokenAddresses[5]);
+
+          payout = await core.token.checkUBIPayout(accounts[5], {
+            safeAddress: safeAddresses[5],
+          });
+        });
+
+        it('should add the next payout to our balance', async () => {
+          const balanceBefore = await token.methods
+            .balanceOf(safeAddresses[5])
+            .call();
+
+          await core.token.requestUBIPayout(accounts[5], {
+            safeAddress: safeAddresses[5],
+          });
+
+          const balanceAfter = await token.methods
+            .balanceOf(safeAddresses[5])
+            .call();
+
+          const expectedBalance = web3.utils
+            .toBN(balanceBefore)
+            .add(payout)
+            .toString();
+
+          // Do not check for the exact amount as payout is changing every second
+          expect(web3.utils.toBN(balanceAfter).gt(expectedBalance)).toBe(true);
+        });
       });
     });
   });
