eb342f621b130971d175f8deb593faa57105e8db
Andreas Dzialocha <x12@adz.garden>
2020-09-30 14:41:09 +0200

    Uncomment tests again

--- test/safe.test.js
+++ test/safe.test.js
@@ -52,162 +52,162 @@ describe('Safe', () => {
       expect(safeAddressAgain).toBe(safeAddress);
     });
 
-    // it('should be able to manually fund it for deployment', async () => {
-    //   expect(
-    //     await core.safe.isFunded(accounts[0], {
-    //       safeAddress,
-    //     }),
-    //   ).toBe(false);
+    it('should be able to manually fund it for deployment', async () => {
+      expect(
+        await core.safe.isFunded(accounts[0], {
+          safeAddress,
+        }),
+      ).toBe(false);
 
-    //   await fundSafe(accounts[0], safeAddress);
+      await fundSafe(accounts[0], safeAddress);
 
-    //   expect(
-    //     await core.safe.isFunded(accounts[0], {
-    //       safeAddress,
-    //     }),
-    //   ).toBe(true);
+      expect(
+        await core.safe.isFunded(accounts[0], {
+          safeAddress,
+        }),
+      ).toBe(true);
 
-    //   const result = await core.safe.deploy(accounts[0], {
-    //     safeAddress,
-    //   });
+      const result = await core.safe.deploy(accounts[0], {
+        safeAddress,
+      });
 
-    //   // .. wait for Relayer to really deploy Safe
-    //   await loop(() => web3.eth.getCode(safeAddress));
+      // .. wait for Relayer to really deploy Safe
+      await loop(() => web3.eth.getCode(safeAddress));
 
-    //   // Deploy Token as well to pay our fees later
-    //   await core.token.deploy(accounts[0], {
-    //     safeAddress,
-    //   });
+      // Deploy Token as well to pay our fees later
+      await core.token.deploy(accounts[0], {
+        safeAddress,
+      });
 
-    //   expect(result).toBe(true);
-    // });
+      expect(result).toBe(true);
+    });
   });
 
-  // describe('when a new Safe gets created through collecting trust connections', () => {
-  //   let safeAddress;
-  //   let trustAccounts;
-  //   let trustSafeAddresses;
+  describe('when a new Safe gets created through collecting trust connections', () => {
+    let safeAddress;
+    let trustAccounts;
+    let trustSafeAddresses;
 
-  //   beforeAll(async () => {
-  //     trustAccounts = [accounts[1], accounts[2], accounts[3]];
-  //     trustSafeAddresses = [];
+    beforeAll(async () => {
+      trustAccounts = [accounts[1], accounts[2], accounts[3]];
+      trustSafeAddresses = [];
 
-  //     const safeCreationNonce = new Date().getTime();
+      const safeCreationNonce = new Date().getTime();
 
-  //     safeAddress = await core.safe.prepareDeploy(accounts[0], {
-  //       nonce: safeCreationNonce,
-  //     });
+      safeAddress = await core.safe.prepareDeploy(accounts[0], {
+        nonce: safeCreationNonce,
+      });
 
-  //     // Create manually funded accounts for trust connections
-  //     const tasks = trustAccounts.map((account) => {
-  //       return deploySafeAndToken(core, account);
-  //     });
+      // Create manually funded accounts for trust connections
+      const tasks = trustAccounts.map((account) => {
+        return deploySafeAndToken(core, account);
+      });
 
-  //     const results = await Promise.all(tasks);
-  //     results.forEach((result) => {
-  //       trustSafeAddresses.push(result.safeAddress);
-  //     });
-  //   });
+      const results = await Promise.all(tasks);
+      results.forEach((result) => {
+        trustSafeAddresses.push(result.safeAddress);
+      });
+    });
 
-  //   it('should get funded through the relayer', async () => {
-  //     // It should not be funded
-  //     expect(
-  //       await core.safe.isFunded(accounts[0], {
-  //         safeAddress,
-  //       }),
-  //     ).toBe(false);
+    it('should get funded through the relayer', async () => {
+      // It should not be funded
+      expect(
+        await core.safe.isFunded(accounts[0], {
+          safeAddress,
+        }),
+      ).toBe(false);
 
-  //     // Receive 3 incoming trust connections from other users
-  //     const connectionTasks = trustAccounts.map((account, index) => {
-  //       return addTrustConnection(core, account, {
-  //         user: safeAddress,
-  //         canSendTo: trustSafeAddresses[index],
-  //         limitPercentage: 10,
-  //       });
-  //     });
+      // Receive 3 incoming trust connections from other users
+      const connectionTasks = trustAccounts.map((account, index) => {
+        return addTrustConnection(core, account, {
+          user: safeAddress,
+          canSendTo: trustSafeAddresses[index],
+          limitPercentage: 10,
+        });
+      });
 
-  //     await Promise.all(connectionTasks);
+      await Promise.all(connectionTasks);
 
-  //     // Deploy Safe
-  //     await core.safe.deploy(accounts[0], {
-  //       safeAddress,
-  //     });
+      // Deploy Safe
+      await core.safe.deploy(accounts[0], {
+        safeAddress,
+      });
 
-  //     // .. wait for Relayer to really deploy Safe
-  //     await loop(() => web3.eth.getCode(safeAddress));
+      // .. wait for Relayer to really deploy Safe
+      await loop(() => web3.eth.getCode(safeAddress));
 
-  //     // Deploy Token
-  //     await core.token.deploy(accounts[0], {
-  //       safeAddress,
-  //     });
+      // Deploy Token
+      await core.token.deploy(accounts[0], {
+        safeAddress,
+      });
 
-  //     const tokenAddress = await core.token.getAddress(accounts[0], {
-  //       safeAddress,
-  //     });
+      const tokenAddress = await core.token.getAddress(accounts[0], {
+        safeAddress,
+      });
 
-  //     const code = await web3.eth.getCode(tokenAddress);
-  //     expect(code).not.toBe('0x');
-  //     expect(web3.utils.isAddress(tokenAddress)).toBe(true);
-  //   });
-  // });
+      const code = await web3.eth.getCode(tokenAddress);
+      expect(code).not.toBe('0x');
+      expect(web3.utils.isAddress(tokenAddress)).toBe(true);
+    });
+  });
 
-  // describe('when I want to manage the owners of a Safe', () => {
-  //   let safeAddress;
+  describe('when I want to manage the owners of a Safe', () => {
+    let safeAddress;
 
-  //   beforeAll(async () => {
-  //     const result = await deploySafeAndToken(core, accounts[0]);
-  //     safeAddress = result.safeAddress;
-  //   });
+    beforeAll(async () => {
+      const result = await deploySafeAndToken(core, accounts[0]);
+      safeAddress = result.safeAddress;
+    });
 
-  //   it('should return a list of the current owners', async () => {
-  //     const owners = await core.safe.getOwners(accounts[0], {
-  //       safeAddress,
-  //     });
+    it('should return a list of the current owners', async () => {
+      const owners = await core.safe.getOwners(accounts[0], {
+        safeAddress,
+      });
 
-  //     expect(owners[0]).toBe(accounts[0].address);
-  //     expect(owners.length).toBe(1);
-  //   });
+      expect(owners[0]).toBe(accounts[0].address);
+      expect(owners.length).toBe(1);
+    });
 
-  //   it('should add another owner to the Safe', async () => {
-  //     const response = await core.safe.addOwner(accounts[0], {
-  //       safeAddress,
-  //       ownerAddress: accounts[1].address,
-  //     });
+    it('should add another owner to the Safe', async () => {
+      const response = await core.safe.addOwner(accounts[0], {
+        safeAddress,
+        ownerAddress: accounts[1].address,
+      });
 
-  //     expect(web3.utils.isHexStrict(response)).toBe(true);
+      expect(web3.utils.isHexStrict(response)).toBe(true);
 
-  //     const owners = await core.safe.getOwners(accounts[0], {
-  //       safeAddress,
-  //     });
+      const owners = await core.safe.getOwners(accounts[0], {
+        safeAddress,
+      });
 
-  //     expect(owners[0]).toBe(accounts[1].address);
-  //     expect(owners[1]).toBe(accounts[0].address);
-  //     expect(owners.length).toBe(2);
+      expect(owners[0]).toBe(accounts[1].address);
+      expect(owners[1]).toBe(accounts[0].address);
+      expect(owners.length).toBe(2);
 
-  //     await loop(
-  //       () => {
-  //         return core.safe.getAddresses(accounts[0], {
-  //           ownerAddress: accounts[1].address,
-  //         });
-  //       },
-  //       (addresses) => addresses.includes(safeAddress),
-  //     );
-  //   });
+      await loop(
+        () => {
+          return core.safe.getAddresses(accounts[0], {
+            ownerAddress: accounts[1].address,
+          });
+        },
+        (addresses) => addresses.includes(safeAddress),
+      );
+    });
 
-  //   it('should remove an owner from the Safe', async () => {
-  //     const response = await core.safe.removeOwner(accounts[0], {
-  //       safeAddress,
-  //       ownerAddress: accounts[1].address,
-  //     });
+    it('should remove an owner from the Safe', async () => {
+      const response = await core.safe.removeOwner(accounts[0], {
+        safeAddress,
+        ownerAddress: accounts[1].address,
+      });
 
-  //     expect(web3.utils.isHexStrict(response)).toBe(true);
+      expect(web3.utils.isHexStrict(response)).toBe(true);
 
-  //     const owners = await core.safe.getOwners(accounts[0], {
-  //       safeAddress,
-  //     });
+      const owners = await core.safe.getOwners(accounts[0], {
+        safeAddress,
+      });
 
-  //     expect(owners[0]).toBe(accounts[0].address);
-  //     expect(owners.length).toBe(1);
-  //   });
-  // });
+      expect(owners[0]).toBe(accounts[0].address);
+      expect(owners.length).toBe(1);
+    });
+  });
 });
